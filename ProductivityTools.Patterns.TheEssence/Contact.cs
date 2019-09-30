37e6575a0a95f65d58bc0886230ee21075962a3a
pwujczyk <pwujczyk@ecovadis.com>
2019-09-30 18:12:38 +0200

    TheEssence

---
+++ ProductivityTools.Patterns.TheEssence/Contact.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductivityTools.Patterns.TheEssence
+{
+    public class Contact
+    {
+        private string mPostalAddress = string.Empty;   // Optional address value.
+        private string mPhoneNumber = string.Empty;     // Optional phone number value
+        private readonly string mFirstName = string.Empty;       // Copied from essence
+        private readonly string mLastName = string.Empty;        // Copied from essence
+
+        public string LastName => mLastName;
+        public string FirstName => mFirstName;
+
+        public string PostalAddress
+        {
+            get
+            {
+                return mPostalAddress;
+            }
+            set
+            {
+                if (mPostalAddress != value)
+                    mPostalAddress = value;
+            }
+        }
+
+        public string PhoneNumber
+        {
+            get
+            {
+                return mPhoneNumber;
+            }
+            set
+            {
+                if (mPhoneNumber != value)
+                    mPhoneNumber = value;
+            }
+        }
+
+        public class ContactEssence
+        {
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+
+            public ContactEssence() { }
+
+            public Contact CreateContact()
+            {
+                if (this.Validate() == true)
+                    return new Contact(this);
+                return null;
+            }
+
+            private bool Validate()
+            {
+                if (FirstName ==null || FirstName.Length == 0) return false;
+                if (LastName == null || LastName?.Length == 0)return false;
+                return true;
+            }
+        }
+
+        // Private constructor can only be accessed by internal classes
+        private Contact(Contact.ContactEssence xEssence)
+        {
+            // Copy required, immutable properties from essence
+            mFirstName = xEssence.FirstName;
+            mLastName = xEssence.LastName;
+        }
+
+        // We make the default constructor private and mark it as obsolete.
+        [System.Obsolete("This constructor is obsolete -- use the ContactEssence class to create Contacts", true)]
+        private Contact()
+        {
+        }
+
+        public override string ToString()
+        {
+            System.Text.StringBuilder outString = new System.Text.StringBuilder();
+            System.IFormatProvider format = System.Globalization.CultureInfo.CurrentUICulture;
+
+            outString.AppendFormat(format, "Name : {0} {1}\n", this.FirstName, this.LastName);
+            outString.AppendFormat(format, "Phone: {0}\n", this.PhoneNumber);
+            outString.AppendFormat(format, "Address: {0}\n", this.PostalAddress);
+
+            return outString.ToString();
+        }
+    }
+}
--- ProductivityTools.Patterns.TheEssence/Program.cs
+++ ProductivityTools.Patterns.TheEssence/Program.cs
@@ -6,7 +6,31 @@ namespace ProductivityTools.Patterns.TheEssence
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Contact.ContactEssence essence = new Contact.ContactEssence();
+
+            // Cannot create Contact yet ...
+            Contact invalidContact = essence.CreateContact();
+            if (invalidContact == null)
+            {
+                Console.WriteLine("Created NULL contact!");
+            }
+
+            // Fill in the rest of the required fields
+            essence.FirstName = "Dermot";
+            essence.LastName = "Gubbins";
+
+            // Try creating contact again
+            Contact validContact = essence.CreateContact();
+            // Add a phone number (optional)
+            // but not an address.
+            validContact.PhoneNumber = "(+44) 444 444";
+
+            // Display the valid contact.
+            Console.WriteLine("\nContact Created ...\n");
+            Console.WriteLine(validContact.ToString());
+
+            Console.WriteLine("Press RETURN to end");
+            Console.ReadLine();
         }
     }
 }
