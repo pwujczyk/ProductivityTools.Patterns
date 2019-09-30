47b93b997feba222219a5b912bf89af41e3f9e5d
pwujczyk <pwujczyk@ecovadis.com>
2019-09-30 14:52:03 +0200

    initial

---
+++ ProductivityTools.Patterns.TheEssence/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProductivityTools.Patterns.TheEssence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
