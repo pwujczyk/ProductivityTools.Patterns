f4bc173bc9d26c93fc4a45de73489e20b079a7d3
pwujczyk <pwujczyk@ecovadis.com>
2019-10-19 09:22:16 +0200

    observer

---
+++ ProductivityTools.Patterns.Observer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProductivityTools.Patterns.Observer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
